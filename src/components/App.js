f6c6b5c408f6424385a5aac835fe15361f469bb4
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-07-26 10:15:41 +0200

    add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main/>
+      <Footer />
     </div>
   );
 }
