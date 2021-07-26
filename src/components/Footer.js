32c08b5133ae641104099ca80d43e5b5984832ba
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-07-26 12:56:04 +0200

    add footer first version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='footer' id='contact'>
-        <div className="footer__copy">
-            <p>© 2021 · Developed by Patricia Vaquero with ♡ · </p>
-        </div>
+    <footer className="footer" id="contact">
+      <div className="footer__container">
+        <h2 className='footer__title'>&#60;contact&#62;</h2>
+        <div className="footer__rrss"></div>
+      </div>
+      <div className="footer__copy">
+        <p>© 2021 · Developed by Patricia Vaquero with ♡ · </p>
+      </div>
     </footer>
   );
 };
