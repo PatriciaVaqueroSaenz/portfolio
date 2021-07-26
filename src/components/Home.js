4efa8ea9edde974d22e955698923f4e635e442bb
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-07-26 13:46:00 +0200

    add typewritter effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+
 const Home = () => {
   return (
     <section id="home" className="home">
       <h2 className="home__title">&#60;home&#62;</h2>
+      <div className="home__container">
+        <div className="home__container--text">
+          <p className="home__text">Hi! Welcome to my portfolio... Hope you enjoy it!</p>
+        </div>
+      </div>
     </section>
   );
 };
