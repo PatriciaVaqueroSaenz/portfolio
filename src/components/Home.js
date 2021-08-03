64ae58826ad046953fc415a797f3b60ec1a149e6
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-08-03 11:05:51 +0200

    change home text position

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,7 @@ const Home = () => {
     <section id="home" className="home">
       <h2 className="home__title">&#60;home&#62;</h2>
       <div className="home__container">
-        {/* <div className='home__container--img'>
-          <img className="home__container--image" src={photo} alt="Patricia Vaquero" />
-        </div> */}
-        <div className="home__container--text">
           <p className="home__text">Hi! Welcome to my portfolio... Hope you enjoy it!</p>
-        </div>
       </div>
     </section>
   );
