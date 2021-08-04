d08b1e4b38bb336351d83cf23c36107b8412a12c
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-08-04 08:34:34 +0200

    add footer link to home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from 'react-scroll';
+import circle from "../images/circle.png"
 
 const Footer = () => {
   return (
     <footer className="footer" id="contact">
       <div className="footer__container">
         <h2 className='footer__title'>&#60;contact&#62;</h2>
-        <div className="footer__rrss"></div>
+        <div className="footer__rrss">
+          <p className="footer__rrss--text">Let's keep in touch!</p>
+          <ul className="footer__rrss--list">
+            <li>Linkedin</li>
+            <li>Github</li>
+            <li>Email</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
+      </div>
+      <div className='footer__link--container'>
+          <Link
+            to="home"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            duration={600}
+          >
+            <img className="footer__link" src={circle} alt="Go Home" />
+          </Link>
       </div>
       <div className="footer__copy">
         <p>© 2021 · Developed by Patricia Vaquero with ♡ · </p>
