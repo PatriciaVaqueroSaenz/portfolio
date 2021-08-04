ab4aa73d3c3df6d8c950d793db29a123301b154d
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-08-04 09:25:43 +0200

    add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { Link } from 'react-scroll';
-import circle from "../images/circle.png"
+import { Link } from "react-scroll";
+import circle from "../images/circle.png";
+import logo from "../images/circlelogo.png";
 
 const Footer = () => {
   return (
     <footer className="footer" id="contact">
       <div className="footer__container">
-        <h2 className='footer__title'>&#60;contact&#62;</h2>
+        <h2 className="footer__title">&#60;contact&#62;</h2>
+        <div className="footer__content">
+          <img
+            className="footer__content--logo"
+            src={logo}
+            alt="logo Patricia Vaquero"
+          />
+          <p className="footer__content--text">Let's keep in touch!</p>
+        </div>
+
         <div className="footer__rrss">
-          <p className="footer__rrss--text">Let's keep in touch!</p>
           <ul className="footer__rrss--list">
-            <li>Linkedin</li>
-            <li>Github</li>
-            <li>Email</li>
-            <li>Twitter</li>
+            <li>
+              <a href="https://www.linkedin.com/in/patriciavaquerosaenz">
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/PatriciaVaqueroSaenz">Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/patriciavaquerosaenz">
+                Email
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/pati_vaquero">Twitter</a>
+            </li>
           </ul>
         </div>
       </div>
-      <div className='footer__link--container'>
-          <Link
-            to="home"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            duration={600}
-          >
-            <img className="footer__link" src={circle} alt="Go Home" />
-          </Link>
+      <div className="footer__link--container">
+        <Link
+          to="home"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          duration={600}
+        >
+          <img className="footer__link" src={circle} alt="Go Home" />
+        </Link>
       </div>
       <div className="footer__copy">
         <p>© 2021 · Developed by Patricia Vaquero with ♡ · </p>
