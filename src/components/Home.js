260ce3edd12115ad73a43552f78c59b617804907
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-08-10 11:42:08 +0200

    add second sentence

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ const Home = () => {
     <section id="home" className="home">
       <h2 className="home__title">&#60;home&#62;</h2>
       <div className="home__container">
-          <p className="home__text">Hi! Welcome to my portfolio... Hope you enjoy it!</p>
+          <p className="home__text-1">Hi! I´m Patricia Vaquero and I´m frontend developer</p>
+          <p className="home__text-2">Welcome to my portfolio... Hope you enjoy it!</p>
       </div>
     </section>
   );
